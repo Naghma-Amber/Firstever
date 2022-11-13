bce7e5bd01b3b67bd9d3a03451bc111e1af2c249
Naghma-Amber <100185627+Naghma-Amber@users.noreply.github.com>
2022-11-13 15:42:47 +0530

    Create naghma.py

---
+++ naghma.py
@@ -0,0 +1,10 @@
+# This program subtract two numbers
+
+num1 = 6.5
+num2 = 6.3
+
+# subtract two numbers
+sum = num1 - num2
+
+# Display the subtraction
+print('The subtraction of {0} and {1} is {2}'.format(num1, num2, sum))
