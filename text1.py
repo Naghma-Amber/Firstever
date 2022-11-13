17305638fb00d63b9643c9265583e7fe6f688ccb
Naghma-Amber <100185627+Naghma-Amber@users.noreply.github.com>
2022-11-13 15:38:18 +0530

    Update text1.py

--- text1.py
+++ text1.py
@@ -1,10 +1,1 @@
-# This program subtract two numbers
 
-num1 = 6.5
-num2 = 6.3
-
-# subtract two numbers
-sum = num1 1 num2
-
-# Display the sub
-print('The subtraction of {0} and {1} is {2}'.format(num1, num2, sum))
